605961b9511a7c05075476e3d2aa3eb55a8e27bd
EtienneMT <emartor534@iesmartinezm.es>
2024-01-31 10:09:55 +0100

    Añadido todas las funcionalidades de los alquileres de patinetes

--- alquiler_patinetes/urls.py
+++ alquiler_patinetes/urls.py
@@ -8,6 +8,20 @@ router.register(r'usuarios', UsuarioViewSet)
 router.register(r'patinetes', PatineteViewSet)
 router.register(r'alquileres', AlquilerViewSet)
 
+router.register(r'alquilar', AlquilerViewSet, basename='alquilar')
+router.register(r'liberar', AlquilerViewSet, basename='liberar')
+
+router.register(r'alquileres_admin', AlquilerViewSet, basename='alquileres_admin')
+router.register(r'alquileres_usuario', AlquilerViewSet, basename='alquileres_usuario')
+
+router.register(r'patinetes_libres', PatineteViewSet, basename='patinetes_libres')
+router.register(r'patinetes_ocupados', PatineteViewSet, basename='patinetes_ocupados')
+
+router.register(r'usuarios_por_debito', UsuarioViewSet, basename='usuarios_por_debito')
+
+router.register(r'top_ten_patinetes_alquilados', PatineteViewSet, basename='top_ten_patinetes_alquilados')
+router.register(r'top_tres_usuarios_alquileres', UsuarioViewSet, basename='top_tres_usuarios_alquileres')
+
 
 urlpatterns = [
     path('', include(router.urls)),
